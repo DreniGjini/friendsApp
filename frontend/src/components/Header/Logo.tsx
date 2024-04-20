2c1a0526a44675ccc0d0f8340bd5ecd197bbeee8
DreniGjini <dreni.g99@gmail.com>
2024-04-21 00:00:18 +0200

    header base

---
+++ frontend/src/components/Header/Logo.tsx
@@ -0,0 +1,11 @@
+import LockIcon from "@heroicons/react/24/outline/LockClosedIcon";
+
+const Logo = () => {
+  return (
+    <div className="w-14 h-14 rounded-full bg-gray-400 flex items-center justify-center">
+      <LockIcon className="w-8 h-8 bg-gray-100 text-white opacity-50" />
+    </div>
+  );
+};
+
+export default Logo;
